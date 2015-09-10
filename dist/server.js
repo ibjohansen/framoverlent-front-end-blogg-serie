b4e1d3103cbb8a93f07d8ad48d1b8730b5f2c934
Ib Johansen <ib.johansen.jr@gmail.com>
2015-09-10 17:44:54 +0200

    edited server file

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,8 @@
 var express = require('express');
-var path = require('path');
 var app = express();
-var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? process.env.PORT : 3000;
 
 app.use(express.static(__dirname));
 
-app.listen(port, function () {
-    console.log('Fremoverlen demo lytter på port ' + port);
+var server = app.listen(3000, function () {
+    console.log('Fremoverlent demo lytter på port %s', server.address().port);
 });
